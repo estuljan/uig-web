c8084c20cfacc21002cacb64cf0fd5bf3d0a3475
asmitul <asmitullah@gmail.com>
2025-11-09 10:39:44 +0300

    rebuild

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders dictionary title', () => {
-  render(<App />);
-  const heading = screen.getByRole('heading', { name: /uyghur dictionary/i });
-  expect(heading).toBeInTheDocument();
-});
